1d17eabe88e411c7a33de83303aacd9608d8abba
Anton Matrosov <redsparkzone@users.noreply.github.com>
2016-09-29 15:34:20 +0300

    Create Action.js

---
+++ Action.js
@@ -0,0 +1,13 @@
+var Action = pc.createScript ('Action');
+
+
+Action.prototype.initialize = function()
+{
+    
+};
+
+
+Action.prototype.doSomething = function ()
+{
+    alert ('Action :: doSomething');
+};
